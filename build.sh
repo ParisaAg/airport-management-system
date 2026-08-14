a10b446c1d15c725828ddab0de4ec400c988617e
ParisaAg <parisaaghaei17@gmail.com>
2026-08-14 16:26:01 +0330

    chore: provision optional production demo data

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ python -m pip install -r requirements/prod.txt
 
 python manage.py collectstatic --no-input --settings=config.settings.prod
 python manage.py migrate --no-input --settings=config.settings.prod
+
+if [[ -n "${DEMO_SEED_PASSWORD:-}" ]]; then
+    python manage.py seed_demo \
+        --settings=config.settings.prod \
+        --password "$DEMO_SEED_PASSWORD"
+fi
